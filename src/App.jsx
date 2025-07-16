1009c5577d32d28d18548face415ed33378d88dd
pranav ranjan <pranavranjan9471@gmail.com>
2025-07-17 00:52:15 +0530

    doing this and learning about data-driven approach and limitations

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Header from "./components/header";
+import Entry from "./components/entry";
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Entry />
+    </>
+  );
+}
