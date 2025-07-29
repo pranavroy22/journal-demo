39ebbd30329941d2e320a2f8cfa4228a4c41dff7
pranav ranjan <pranavranjan9471@gmail.com>
2025-07-30 00:50:01 +0530

    completed the journal with the use of props and its resuability

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/header";
 import Entry from "./components/entry";
+import data from "./data";
 export default function App() {
+  const entryele = data.map((entry) => <Entry key={entry.id} entry={entry} />);
   return (
     <>
       <Header />
-      <Entry />
+      <main className="container">{entryele}</main>
     </>
   );
 }
---
+++ src/in.js
@@ -0,0 +1,17 @@
+//for square of the number
+
+// const num = [1, 2, 3, 4];
+// const prod = num.map((num1) => num1 * num1);
+// console.log(prod);
+
+//first capital letter
+
+// const names = ["alice", "bob", "charlie", "danie"];
+// const cap = names.map((capital) => capital[0].toUpperCase() + capital.slice(1));
+// console.log(cap);
+
+//paragraph tag
+
+// const pokemon = ["Balbasaure", "Pikachu", "Charizard"];
+// const mon = pokemon.map((mons) => `<p>{$mons}</p>`);
+// console.log(mon);
